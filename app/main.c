285de2e9d95568105313b34da840d175b91a15c5
a <airlanggafidiyanto@Airlanggas-MacBook-Pro.local>
2022-04-24 19:57:16 +0700

    test

--- app/main.c
+++ app/main.c
@@ -4,17 +4,10 @@
 
 #define DEVICE "/dev/airlangga"
 
-int main() {
-	int ret, debug = 1, fd = 0, ppos = 0;
-	char buffer[256];
-	char stringToSend[256];
+int debug = 1, fd = 0;
 
-	printf("Demo calling kernel module using character device driver\n");
-	printf("Reading file %s...\n", DEVICE);
-
-	fd = open(DEVICE, O_RDWR);
-
-	int write_length = 0;
+int read_device() {
+	int write_length = 0, ppos = 0, debug = 1;
 	ssize_t ret;
 	char *data = (char *)malloc(1024 * sizeof(char));
 
@@ -33,9 +26,22 @@ int main() {
 			printf("writting failed\n");
 	else
 			printf("writting success\n");
+
 	if (debug)fflush(stdout); /*not to miss any log*/
 	free(data);
-	
+}
+
+int main() {
+	int ret;
+	char buffer[256];
+	char stringToSend[256];
+
+	printf("Demo calling kernel module using character device driver\n");
+	printf("Reading file %s...\n", DEVICE);
+
+	fd = open(DEVICE, O_RDWR);
+	read_device();
 	close(fd);
+	
 	return 0;
 }
